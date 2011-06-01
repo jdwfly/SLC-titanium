1aee2ba99be0f1f694fffa06067703c52482d535
Joe Wheaton <joseph.wheaton@gmail.com>
2011-06-01 16:40:09 -0700

    added main coffee script file

---
+++ Resources/main.coffee
@@ -0,0 +1,2 @@
+tabs = slc.ui.createApplicationTabGroup()
+tabs.open({transition:Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT})
---
+++ Resources/main.js
@@ -0,0 +1,7 @@
+(function() {
+  var tabs;
+  tabs = slc.ui.createApplicationTabGroup();
+  tabs.open({
+    transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+  });
+}).call(this);
